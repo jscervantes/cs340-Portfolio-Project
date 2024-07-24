3feb356440c57092b2cb7c8bc1943e550abfbc2c
Elizabeth <e.ogleznev+github@protonmail.com>
2024-07-23 17:32:58 -0700

    Add j2 files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
 from flask import request
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -8,10 +9,31 @@ app = Flask(__name__)
 # Routes
 @app.route('/')
 def root():
-    return "Ciani Synths"
+    return render_template("main.j2")
+
+@app.route('/synthesizers')
+def synthesizers():
+    return render_template("synthesizers.j2")
+
+@app.route('/manufacturers')
+def manufacturers():
+    return render_template("manufacturers.j2")
+
+@app.route('/customers')
+def customers():
+    return render_template("customers.j2")
+
+@app.route('/orders')
+def orders():
+    return render_template("orders.j2")
+
+@app.route('/purchases')
+def purchases():
+    return render_template("purchases.j2")
 
 # Listener
 if __name__ == "__main__":
 
-    # Start the app on port 3000, it will be different once hosted
+    #Elizabeth port: 49408
+    #Main port: 49484
     app.run(port=49484, debug=True)
