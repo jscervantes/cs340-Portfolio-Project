574ee14de344ec5bd5db0dc6bb2dac9feae4bfb4
Elizabeth <e.ogleznev+github@protonmail.com>
2024-08-01 16:20:32 -0700

    kjdfsafd

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 # Routes
 @app.route('/')
 def root():
-    return render_template("main.j2")
+    title = "Ciani Synths"
+    return render_template("base.j2", title=title)
 
 @app.route('/synthesizers', methods=('GET', 'POST'))
 def synthesizers():
@@ -21,7 +22,69 @@ def manufacturers():
 
 @app.route('/customers', methods=('GET', 'POST'))
 def customers():
-    return render_template("customers.j2")
+    customers = [
+          {
+            "id": "1",
+            "firstname": "Naomi",
+            "lastname": "Shaw",
+            "street": "596 Edgefield Drive",
+            "street2": "NULL",
+            "city": "Clarksburg",
+            "state": "WV",
+            "zip": "26301",
+            "email": "likvidatorcelok@ezsmail.com",
+            "phone": "7188904009"
+        },
+          {
+            "id": "2",
+            "firstname": "Brendan",
+            "lastname": "Frye",
+            "street": "3308 Godfrey Street",
+            "street2": "NULL",
+            "city": "Portland",
+            "state": "OR",
+            "zip": "97229",
+            "email": "fromponmachineus@jojoo.online",
+            "phone": "7188904009"
+        },
+          {
+            "id": "3",
+            "firstname": "Calvin",
+            "lastname": "Melton",
+            "street": "103 Mutton Town Road",
+            "street2": "NULL",
+            "city": "Vancouver",
+            "state": "WA",
+            "zip": "98663",
+            "email": "someong6n9v@bankinnepal.com",
+            "phone": "3051178216"
+        },
+          {
+            "id": "4",
+            "firstname": "Lacey",
+            "lastname": "Robbins",
+            "street": "3823 Heron Way",
+            "street2": "NULL",
+            "city": "Portland",
+            "state": "OR",
+            "zip": "97205",
+            "email": "64j0cy5q5@dunilakeling.com",
+            "phone": "3051178216"
+        },
+          {
+            "id": "5",
+            "firstname": "Dana",
+            "lastname": "Greer",
+            "street": "1225 New Street",
+            "street2": "NULL",
+            "city": "Eugene",
+            "state": "OR",
+            "zip": "97401",
+            "email": "parapamm@bmuss.com",
+            "phone": "7775332610"
+        },
+        ]
+    return render_template("customers.j2", customers = customers)
 
 @app.route('/orders', methods=('GET', 'POST'))
 def orders():
